0385d3c6422dfa34e282f376071d87053b14b99e
vijiraju <lakshmiraju96543@gmail.com>
2026-05-21 23:16:34 +0530

    added all components

--- book_recommender/config/configuration.py
+++ book_recommender/config/configuration.py
@@ -6,6 +6,8 @@ from book_recommender.exception.exception_handler import AppException
 from book_recommender.entity.config_entity import DataIngestionConfig
 from book_recommender.entity.config_entity import DataValidationConfig
 from book_recommender.entity.config_entity import DataTransformationConfig
+
+from book_recommender.entity.config_entity import ModelTrainerConfig
 from book_recommender.constant import *
 
 class AppConfiguration:
@@ -81,6 +83,32 @@ class AppConfiguration:
             )
 
             logging.info(f"Data Transformation Config: {response}")
+            return response
+
+        except Exception as e:
+            raise AppException(e, sys) from e
+        
+    def get_model_trainer_config(self) -> ModelTrainerConfig:
+        try:
+            model_trainer_config = self.configs_info['model_trainer_config']
+            data_transformation_config = self.configs_info['data_transformation_config']
+            data_ingestion_config = self.configs_info['data_ingestion_config']
+            dataset_dir = data_ingestion_config['dataset_dir']
+            artifacts_dir = self.configs_info['artifacts_config']['artifacts_dir']
+
+            
+            
+            transformed_data_file_dir = os.path.join(artifacts_dir, dataset_dir, data_transformation_config['transformed_data_dir'], 'transformed_data.pkl')
+            trained_model_dir = os.path.join(artifacts_dir, model_trainer_config['trained_model_dir'])
+            trained_model_name = model_trainer_config['trained_model_name']
+
+            response = ModelTrainerConfig(
+                transformed_data_file_dir = transformed_data_file_dir,
+                trained_model_dir = trained_model_dir,
+                trained_model_name = trained_model_name
+            )
+
+            logging.info(f"Model Trainer Config: {response}")
             return response
 
         except Exception as e:
--- book_recommender/pipeline/training_pipeline.py
+++ book_recommender/pipeline/training_pipeline.py
@@ -1,14 +1,17 @@
 from book_recommender.components.stage_00_data_ingestion import DataIngestion
 from book_recommender.components.stage_01_data_validation import DataValidation
 from book_recommender.components.stage_02_data_transformation import DataTransformation
+from book_recommender.components.stage_03_model_trainer import ModelTrainer
 
 class TrainingPipeline:
     def __init__(self):
         self.data_ingestion=DataIngestion()
         self.data_validation=DataValidation()
         self.data_transformation=DataTransformation()
+        self.model_trainer=ModelTrainer()
     def start_training_pipeline(self):
         self.data_ingestion.initiate_data_ingestion()
 
         self.data_validation.initiate_data_validation()
         self.data_transformation.initiate_data_transformation()
+        self.model_trainer.initiate_model_trainer()
